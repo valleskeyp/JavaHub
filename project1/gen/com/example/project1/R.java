7f35131eff57cf8cf7df6b13bbe19726352c32fe
Peter Valleskey <player1122@gmail.com>
2012-10-25 12:40:46 -0700

    text field work
    looking into out how to keep a blank field from crashing on button push

--- project1/gen/com/example/project1/R.java
+++ project1/gen/com/example/project1/R.java
@@ -23,6 +23,10 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f030000;
         public static final int hello_world=0x7f030001;
+        public static final int item1=0x7f030004;
+        public static final int item2=0x7f030005;
+        public static final int item3=0x7f030006;
+        public static final int item4=0x7f030007;
         public static final int menu_settings=0x7f030002;
         public static final int title_activity_main=0x7f030003;
     }
