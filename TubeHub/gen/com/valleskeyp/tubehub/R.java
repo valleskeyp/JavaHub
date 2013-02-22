2bc49751c1ab208a9db8e6842da6dd312186e030
Peter Valleskey <player1122@gmail.com>
2013-02-21 20:17:47 -0800

    speechless
    the amount of butt kicking this app has done to me is astounding.
    finally make progress after 30+ hours?  sheesh

--- TubeHub/gen/com/valleskeyp/tubehub/R.java
+++ TubeHub/gen/com/valleskeyp/tubehub/R.java
@@ -20,39 +20,35 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int Logout=0x7f08001c;
+        public static final int Logout=0x7f080018;
         public static final int category_input=0x7f080003;
         public static final int description_input=0x7f080005;
         public static final int editText1=0x7f080002;
-        public static final int edit_button=0x7f08001a;
+        public static final int edit_button=0x7f080016;
         public static final int edit_submit=0x7f08000f;
-        public static final int expandableListView1=0x7f080018;
+        public static final int expandableListView1=0x7f080014;
         public static final int geo_false=0x7f08000a;
         public static final int geo_input=0x7f080008;
         public static final int geo_true=0x7f080009;
-        public static final int imageView1=0x7f080010;
-        public static final int imageView2=0x7f080016;
+        public static final int imageView1=0x7f080011;
+        public static final int imageView2=0x7f080012;
         public static final int keyword_input=0x7f080006;
-        public static final int login_button=0x7f080015;
-        public static final int menu_logout=0x7f08001d;
-        public static final int menu_record=0x7f08001f;
-        public static final int menu_search=0x7f08001e;
-        public static final int menu_settings=0x7f08001b;
-        public static final int menu_upload=0x7f080020;
+        public static final int menu_logout=0x7f080019;
+        public static final int menu_record=0x7f08001b;
+        public static final int menu_search=0x7f08001a;
+        public static final int menu_settings=0x7f080017;
+        public static final int menu_upload=0x7f08001c;
         public static final int private_false=0x7f08000e;
         public static final int private_input=0x7f08000c;
         public static final int private_true=0x7f08000d;
-        public static final int record_button=0x7f080019;
+        public static final int record_button=0x7f080015;
         public static final int spinner1=0x7f080004;
-        public static final int temp_button=0x7f080017;
+        public static final int temp_button=0x7f080013;
         public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080011;
-        public static final int textView3=0x7f080013;
         public static final int textview11=0x7f080007;
         public static final int textview12=0x7f08000b;
         public static final int title_input=0x7f080001;
-        public static final int user_account=0x7f080012;
-        public static final int user_password=0x7f080014;
+        public static final int webView=0x7f080010;
     }
     public static final class layout {
         public static final int edit_activity=0x7f030000;
